11980684b9e54209abdac2d333a9169880b2f26a
damn <damn@sebangsas-Mac-mini.local>
2017-08-30 20:40:50 +0700

    add test all components

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,20 +1,86 @@
 import React from 'react';
 
-import { shallow, mount, render } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
 import If from '../If';
+import When from '../When';
+import Otherwise from '../Otherwise';
+import Choose from '../Choose';
 
-describe('Shallow rendering', () => {
-
-  it('just test', () => {
-    const wrapper = shallow(
-      <If condition={false}>
-        <span>hai</span>
-      </If>
+describe('<If />', () => {
+  it('should render children when condition is true', () => {
+    const Children = <span>Hello</span>
+    const wrapper = mount(
+      <If condition={true}>{Children}</If>
     );
-    expect(wrapper).to.not.be.null;
+    expect(wrapper.contains(Children)).to.equal(true);
   });
 
+  it('should not render children when condition is false', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <If condition={false}>{Children}</If>
+    );
+    expect(wrapper.contains(Children)).to.equal(false);
+  });
+});
+
+describe('<When />', () => {
+  it('should render children when condition is true', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <When condition={true}>{Children}</When>
+    );
+    expect(wrapper.contains(Children)).to.equal(true);
+  });
+
+  it('should not render children when condition is false', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <When condition={false}>{Children}</When>
+    );
+    expect(wrapper.contains(Children)).to.equal(false);
+  });
+});
+
+describe('<Otherwise />', () => {
+  it('should render children when condition is true', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <Otherwise condition={true}>{Children}</Otherwise>
+    );
+    expect(wrapper.contains(Children)).to.equal(true);
+  });
+
+  it('should render children when condition is false', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <Otherwise condition={false}>{Children}</Otherwise>
+    );
+    expect(wrapper.contains(Children)).to.equal(true);
+  });
+});
+
+describe('<Choose />', () => {
+  it('should not accept span', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <Choose>
+        {Children}</Choose>
+    );
+    expect(wrapper.contains(Children)).to.equal(false);
+  });
+  it('should accept When', () => {
+    const Children = <span>Hai</span>
+    const wrapper = mount(
+      <Choose>
+        <When condition={true}>
+          {Children}
+        </When>
+      }</Choose>
+    );
+    expect(wrapper.contains(Children)).to.equal(true);
+  });
 });
