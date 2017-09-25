92526991b69e52db647ec4c296cf014bfff90260
Damaera <damaera@live.com>
2017-09-25 19:54:47 +0700

    add For

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var If = require('./dist/If')
+var For = require('./dist/For')
 var When = require('./dist/When')
 var Choose = require('./dist/Choose')
 var Otherwise = require('./dist/Otherwise')
 
 exports.If = If.default
+exports.For = For.default
 exports.When = When.default
 exports.Choose = Choose.default
 exports.Otherwise = Otherwise.default
